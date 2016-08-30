ee999cc4040ccdd6e446262f79a82b10543ef9d3
yongsheng.kuang <yongsheng.kuang@ptthink.com>
2016-08-30 21:29:48 +0800

    完善

--- app/task/taskList.component.ts
+++ app/task/taskList.component.ts
@@ -13,129 +13,8 @@ export const CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR: any = {
 @Component({
     moduleId: module.id,
     selector: 'agl-task-list-status-operate',
-    styles: [`
-        .task-status{
-        position:relative;
-        cursor: pointer;
-        }
-        .task-status .task-status-icon{
-            line-height: 32px;
-        }
-
-        @keyframes broadcast{
-            0%{
-                width: 16px;
-                height:16px;
-                margin-left:0; 
-                margin-top:-8px; 
-                opacity: 1;
-            }
-            10%{
-                width: 32px;
-                height:32px;
-                margin-left:-8px;
-                margin-top:-16px; 
-                opacity: 0;
-            }
-            11%{
-                width: 16px;
-                height:16px;
-                margin-left:0; 
-                margin-top:-8px; 
-                opacity: 0;
-            }
-            12%{
-                width: 16px;
-                height:16px;
-                margin-left:0; 
-                margin-top:-8px; 
-                opacity: 1;
-                }
-            20%{width: 32px;
-                height:32px;
-                margin-left:-8px; 
-                margin-top:-16px;  
-                opacity: 0;}
-            21%{
-                width: 16px;
-                height:16px;
-                margin-left:0; 
-                margin-top:-8px; 
-                opacity: 0;}
-            22%{
-                width: 16px;
-                height:16px;
-                margin-left:0; 
-                margin-top:-8px; 
-                opacity: 1;}
-            30%{
-                width: 32px;
-                height:32px;
-                margin-left:-8px; 
-                margin-top:-16px;  
-                opacity: 0
-            }
-            31%{  
-                width: 16px;
-                height:16px;
-                margin-left:0; 
-                margin-top:-8px; 
-                opacity: 0;
-            }
-            100%{   
-                width: 16px;
-                height:16px;
-                margin-left:0; 
-                margin-top:-8px; 
-                opacity: 1;
-            }
-        }
-
-        .task-status-icon.text-danger~.task-status-broadcast{
-            position: absolute;
-            top:50%;
-            left:0;
-            width: 16px;
-            height:16px;
-            margin-left:0; 
-            margin-top:-8px; 
-
-            border: 1px #a94442 solid;
-            border-radius: 100%;
-            animation: broadcast 12s;
-            animation-iteration-count: infinite;
-        }
-        td textarea.form-control{
-            height:34px;
-            max-width:100%;
-        }
-
-
-        .table >tbody >tr > td.td-operate{
-            padding: 0;
-        }
-        .table >tbody >tr > td.td-operate button.btn{
-            border: none;
-            border-radius: 0px;
-        }
-        .list-group{
-            z-index: 10;
-            position:absolute;
-            top:100%;
-            left:0;
-            display: none;
-                margin:0;
-        }
-        .task-status.active .list-group{
-            display: block;
-        }
-       
-        .list-group .list-group-item{
-            padding: 6px 12px 6px 12px;
-            cursor: pointer;
-        }
-    `],
-    templateUrl:'taskStatusOperate.component.html',
+    styleUrls:['./taskStatusOperate.component.css'],
+    templateUrl:'./taskStatusOperate.component.html',
     providers:[CUSTOM_INPUT_CONTROL_VALUE_ACCESSOR]
 }) class TaskStatusOperateComponent implements ControlValueAccessor {
     private innerValue: any = '';
@@ -191,7 +70,7 @@ const expirationTime: number = 1000 * 60 * 60 * 24 * 2;//2天为到期提醒时�
     moduleId: module.id,
     selector: 'agl-task-list',
     styleUrls: ['./taskList.component.css'],
-    templateUrl: 'taskList.component.html',
+    templateUrl: './taskList.component.html',
     directives: [TaskStatusOperateComponent,FORM_DIRECTIVES]
 })
 export class TaskListComponent implements OnInit {
