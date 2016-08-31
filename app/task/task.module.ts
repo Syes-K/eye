82366d9a659be4f968980fa069cdd7a77456400a
yongsheng.kuang <yongsheng.kuang@ptthink.com>
2016-08-31 10:28:23 +0800

    分离taskStatusOperate 和 TaskList 组件

--- app/task/task.module.ts
+++ app/task/task.module.ts
@@ -4,14 +4,15 @@ import {TaskService} from './task.service';
 import {TaskStatusTextPipe} from './task.status.pipe';
 import {TaskStartComponent} from './taskStart.component';
 import {TaskListComponent} from './taskList.component';
+import{TaskStatusOperateComponent} from './taskStatusOperate.component'
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms/src/form_providers';
+import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, routing],
     exports: [TaskComponent],
-    declarations: [TaskComponent, TaskListComponent,TaskStartComponent, TaskStatusTextPipe],
+    declarations: [TaskComponent, TaskListComponent,TaskStartComponent,TaskStatusOperateComponent, TaskStatusTextPipe],
     providers: [TaskService],
 })
 export class TaskModule { }
